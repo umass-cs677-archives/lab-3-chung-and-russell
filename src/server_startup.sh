da088742df92b56b7f38754bff7ffe5682f2620c
russell-lee <mail.russelllee@gmail.com>
2019-04-20 23:54:23 -0400

    fix unix line endings in .sh file
    also updating replica port for catalog

--- src/server_startup.sh
+++ src/server_startup.sh
@@ -3,4 +3,4 @@ echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5001/tcp; python Order.py'
 echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5002/tcp; python Catalog.py' | ssh $1@elnux2.cs.umass.edu &
 echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5003/tcp; python Frontend.py' | ssh $1@elnux3.cs.umass.edu &
 echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5001/tcp; python Order.py' | ssh $1@elnux2.cs.umass.edu &
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5003/tcp; python Catalog.py' | ssh $1@elnux1.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5002/tcp; python Catalog.py' | ssh $1@elnux1.cs.umass.edu &

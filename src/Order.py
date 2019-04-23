65135da4e965bc4dd79cd3417535cd8c52fcafff
russell-lee <mail.russelllee@gmail.com>
2019-04-23 12:38:47 -0400

    replica consistency with primary replica forwarding(unstable)
    configured for edlab testing

--- src/Order.py
+++ src/Order.py
@@ -1,45 +1,21 @@
 from flask import Flask
 from flask import jsonify
 from flask_restful import reqparse, abort, Api, Resource
+from utils import *
 import csv
 import random
 import time
 import requests
+import sys
 
 app = Flask(__name__)
 api = Api(app)
 
-ORDER_FILE = 'order_log.txt'
+app.config["order_db"] = 'order_log.txt'
 SERVER_CONFIG = 'server_config'
 
 PERIODIC_UPDATE = False
 
-#######################################################
-############### Accessing Catalog server  #############
-#######################################################
-
-
-def get_address(address_dict):
-    IP = address_dict['IP']
-    port = address_dict['port']
-    address = 'http://' + IP + ':' + port
-
-with open(SERVER_CONFIG, mode ='r') as server_file:
-    server_dict = {}
-    csv_reader = csv.DictReader(server_file)
-    for row in csv_reader:
-        server_name = row['Server']
-        server_dict[server_name] = {'Machine': row['Machine'],
-                                    'IP': row['IP'],
-                                    'Port': row['Port']}
-    
-    catalog_dict_0 = server_dict['Catalog_0']
-    catalog_address_0 = get_address(catalog_dict_0)
-    catalog_dict_1 = server_dict['Catalog_1']
-    catalog_address_1 = get_address(catalog_dict_1)
-    ORDER_PORT = server_dict['Order_0']['Port']
-
-CATALOG_ADDRESS = catalog_address_0
 
 
 #######################################################
@@ -47,13 +23,13 @@ CATALOG_ADDRESS = catalog_address_0
 #######################################################
 def reset_orders():
     # initialize order database as a local .txt file
-    with open(ORDER_FILE, mode='w') as order_log:
+    with open(app.config["order_db"], mode='w') as order_log:
         fieldnames = ['order_id', 'processing_time','is_successful','catalog_id','title']
         writer = csv.DictWriter(order_log, fieldnames=fieldnames)
         writer.writeheader()
 
 def get_orders_as_dict():
-    with open(ORDER_FILE, mode='r') as csv_file:
+    with open(app.config["order_db"], mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return [row for row in csv_reader]
 
@@ -81,7 +57,7 @@ def unpack_string_into_order(input):
 
 
 def write_order(order):
-    with open(ORDER_FILE, mode='a') as order_log:
+    with open(app.config["order_db"], mode='a') as order_log:
         fieldnames = ['order_id', 'processing_time','is_successful','catalog_id','title']
         writer = csv.DictWriter(order_log, fieldnames=fieldnames)
         writer.writerow(order)
@@ -118,7 +94,21 @@ def decrement_catalog_server(catalog_id):
 def restock_catalog_server(catalog_id):
     r = requests.get(CATALOG_ADDRESS + '/update/' + catalog_id + '/quantity/increase/300')
 
-
+def forward(query, server_name):
+    """
+    Forward the query for the specified server to execute
+    """
+    root_url = get_root_url(app.config.get("server_dict"), server_name)
+    forward_query = string_builder([root_url], query)
+    try:
+        r = requests.put(forward_query)
+        return r.text
+    except requests.exceptions.ConnectionError:
+        # Primary server is down, holds an election and forwards the
+        # request to the new primary
+        print("primary server down")
+        #hold_election()
+        return forward(query, app.config.get("primary"))
 
 ######################################################
 ################ Setup REST resources ################
@@ -154,10 +144,24 @@ class Buy(Resource):
                 print("Bought '"+ title + "' , stock erroneously changed by " + str(stockChange))
                 title = title + errorString
         order = create_order(order_id,processing_time,is_successful,catalog_id,title)
+        # Not the primary replica, forward the write request
+        if app.config.get("primary") != app.config.get("name") and "sync" not in request.path:
+            order_string = package_order_into_string([str(order_id),str(processing_time),str(is_successful),catalog_id,title])
+            query = string_builder([], "write/", order_string)
+            r = forward(query, app.config.get("primary"))
+            return r
         write_order(order)
-        order_string = package_order_into_string([str(order_id),str(processing_time),str(is_successful),catalog_id,title])
+        
         return jsonify(order)
 
+class Notify(Resource):
+    def get(self, primary_name):
+        """
+        Get notified which server is the new primary
+        """
+        app.config["primary"] = primary_name
+        return(primary_name+ " is the new primary")
+    
 
 # 
 class Write(Resource):
@@ -186,10 +190,60 @@ class OrderList(Resource):
 api.add_resource(OrderList, '/orders')
 api.add_resource(Buy, '/buy/<catalog_id>')
 api.add_resource(Write, '/write/<order_string>')
+api.add_resource(Notify, '/notify/<primary_name>')
 
 
 if __name__ == '__main__':
+
+    #######################################################
+    ############### Accessing Catalog server  #############
+    #######################################################
+
+
+    def get_address(address_dict):
+        IP = address_dict['IP']
+        port = address_dict['Port']
+        address = 'http://' + IP + ':' + port
+        return address
+
+    with open(SERVER_CONFIG, mode ='r') as server_file:
+        server_dict = {}
+        csv_reader = csv.DictReader(server_file)
+        for row in csv_reader:
+            server_name = row['Server']
+            server_dict[server_name] = {'Machine': row['Machine'],
+                                        'IP': row['IP'],
+                                        'Port': row['Port'],
+                                        'Replica_ID': row['Replica_num']}
+        
+        catalog_dict_0 = server_dict['Catalog_0']
+        catalog_address_0 = get_address(catalog_dict_0)
+        catalog_dict_1 = server_dict['Catalog_1']
+        catalog_address_1 = get_address(catalog_dict_1)
+        ORDER_PORT_0 = server_dict['Order_0']['Port']
+        ORDER_PORT_1 = server_dict['Order_1']['Port']
+        CATALOG_ADDRESS = catalog_address_0
+
     # reset the order DB when starting the flask app
     reset_orders()
-    app.run(host = '0.0.0.0', port = ORDER_PORT)
+    # config variables
+    app.config["id"] = sys.argv[1]
+    app.config["name"] = "Order_" + app.config["id"]
+    app.config["order_db"] = "order_" + app.config["id"] + "_db.txt"
+
+
+    app.config["server_dict"] = get_server_dict("server_config", ["Client"])
+    # Redundant variable assignment make the calls to this reference shorter
+    server_dict = app.config["server_dict"]
+    replica_names, replica_ids = zip(*get_replicas(server_dict, "Order"))
+    app.config["peer_ids"] = list(replica_ids)
+    # Assume a primary, if the assumed primary isn't in fact running, another
+    # primary will be elected later on
+    app.config["primary"] = "Order_" + str(max(app.config["peer_ids"]))
+    app.config["peer_names"] = list(replica_names)
+    order_ip, order_port = get_id_port(server_dict, "Order", app.config.get("id"))
+    app.config["catalog_name"] = "Catalog_" + app.config["id"]
+    app.config["catalog_address"] = get_address(server_dict[app.config["catalog_name"]])
+    app.run(host = order_ip, port = order_port)
+    print("app is running??")
 
--- src/server_startup.sh
+++ src/server_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5001/tcp; python Order.py' | ssh $1@elnux1.cs.umass.edu &
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5002/tcp; python Catalog.py' | ssh $1@elnux2.cs.umass.edu &
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5003/tcp; python Frontend.py' | ssh $1@elnux3.cs.umass.edu &
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5001/tcp; python Order.py' | ssh $1@elnux2.cs.umass.edu &
-echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 5002/tcp; python Catalog.py' | ssh $1@elnux1.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 6001/tcp; python3 Order.py 0' | ssh $1@elnux7.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 6002/tcp; python3 Catalog.py 0' | ssh $1@elnux2.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 6003/tcp; python3 Frontend.py' | ssh $1@elnux3.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 6005/tcp; python3 Order.py 1' | ssh $1@elnux2.cs.umass.edu &
+echo 'cd cs677/lab-3-chung-and-russell/src ; fuser -k 6006/tcp; python3 Catalog.py 1' | ssh $1@elnux1.cs.umass.edu
